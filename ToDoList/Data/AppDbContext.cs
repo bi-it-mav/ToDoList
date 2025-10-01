4615e37f424a7f45e0c2306210bc928042054c65
Vladimir Marko <vladimirmarko1988@gmail.com>
2025-10-01 08:21:44 +0200

    Add ToDo management features and EF Core integration
    Introduced a `ToDo` model and `AppDbContext` for database management using Entity Framework Core with MySQL support. Updated `ToDoList.csproj` to include necessary EF Core dependencies.
    
    Added new Razor components: `Create`, `Display`, `List`, and `Modify` for task creation, display, listing, and modification. Updated `Home.razor` to integrate `Create` and `List` components.
    
    Modified `App.razor` to enable interactive server-side rendering. Updated `_Imports.razor` to include additional namespaces for components and models.
    
    Updated the solution file to reflect Visual Studio version changes.

---
+++ ToDoList/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ToDo> ToDos { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
+    }
+}
---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastModifiedAt { get; set; } = DateTime.Now;
+        public bool Done { get; set; } = false;
+        public string Description { get; set; } = "";
+    }
+}
