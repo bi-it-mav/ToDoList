3066daa9fcb2f4b1e55e2159c148673f3fbb8197
Vladimir Marko <vladimirmarko1988@gmail.com>
2025-11-11 02:00:52 +0100

    Change StateChangeSubscriber to a C# class

---
+++ ToDoList/StateChangeSubscriber.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using ToDoList.Services;
+
+namespace ToDoList
+{
+    public abstract class StateChangeSubscriber : ComponentBase, IDisposable
+    {
+        [Inject]
+        // `required` does not actually guarantee anything here
+        public required StateChangeNotifier StateChangeNotifier { get; set; }
+
+        private readonly List<IDisposable> _disposables = [];
+
+        public async Task StateChangeSubscribeAsync(Type topic, Func<Task> updateState)
+        {
+            _disposables.Add(await StateChangeNotifier.SubscribeAsync(topic, async () => {
+                await updateState();
+                StateHasChanged();
+            }));
+        }
+
+        public void Dispose()
+        {
+            _disposables.ForEach(d => d.Dispose());
+            GC.SuppressFinalize(this);
+        }
+    }
+}
