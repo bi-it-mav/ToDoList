f47dde5d80a6d1101a70c131a05af50e7864714b
Vladimir Marko <vladimirmarko1988@gmail.com>
2025-11-13 14:27:15 +0100

    Show full exception messages on exceptions

---
+++ ToDoList/Util.cs
@@ -0,0 +1,17 @@
+﻿namespace ToDoList
+{
+    public static class Util
+    {
+        public static string FullMessage(this Exception? exception)
+        {
+            var messages = new List<string>();
+            var currentException = exception;
+            while (currentException != null)
+            {
+                messages.Add(currentException.Message);
+                currentException = currentException.InnerException;
+            }
+            return string.Join(" → ", messages);
+        }
+    }
+}
